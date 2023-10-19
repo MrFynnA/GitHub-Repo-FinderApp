0b20feeaf9a211b608fdda5a71cf7f8bcc923f31
Mr Fynn <104089609+MrFynnA@users.noreply.github.com>
2023-10-19 07:41:15 +0000

    Update search.jsx

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -18,7 +18,7 @@ const Search=(props)=>{
     <div>
     <form onSubmit={onFindRepo} className={`${styles.Search}  mt-10 max-md:mt-14`}>
  <input type='text' className='max-md:w-[55%] max-md:!text-sm max-md:!pl-4 max-md:!py-4 max-md:placeholder:!text-[1rem]' placeholder='Enter Username' value={userName} onChange={onUserChange}/>
- <button className='max-md:w-[25%] max-md:text-sm max-md:!px-0 max-md:!py-4'>{props.buttonText}</button>
+ <button className='max-md:w-[25%] max-md:text-sm max-md:!px-1 max-md:!py-4'>{props.buttonText}</button>
  </form>
  </div>
  </Fragment>)
