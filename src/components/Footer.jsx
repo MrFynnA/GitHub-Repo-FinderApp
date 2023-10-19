8b25b70b56fd8d4ca70e722df502f027a752a5f2
Mr Fynn <104089609+MrFynnA@users.noreply.github.com>
2023-10-19 07:38:55 +0000

    update to UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='h-[3rem] bg-gradient-to-tl  bg-slate-800 flex items-center justify-start pl-10 max-md:pl-5 text-white font-bold font-mono uppercase text-sm max-md:text-xl  w-full !relative !bottom-0 border-rounded z-50 rounded-md'>
-        <div className='flex items-center italic hover:underline cursor-pointer'><div><a href='https://fynnarthur.netlify.app/message'>{'Get in touch with Fynn here'}</a></div></div>
+        <div className='flex items-center italic hover:underline cursor-pointer max-md:text-sm capitalize'><div><a href='https://fynnarthur.netlify.app/message'>{'Get in touch with Fynn here'}</a></div></div>
     </div>
   )
 }
--- src/components/gitHubUserRepos.jsx
+++ src/components/gitHubUserRepos.jsx
@@ -5,10 +5,10 @@ import MygitSvg from '../assets/MygitSvg'
 const GitHubRepos=(props)=>{
     return(
 <Fragment>
-    <div className={`${styles.content} flex justify-center max-md:mb-2 flex-col my-[2rem] bg-gray-700 shadow-md w-[36%] max-md:w-[92%] pb-[2rem] rounded-lg`}>
+    <div className={`${styles.content} flex justify-center max-md:mb-2 flex-col my-[2rem] bg-gray-700 shadow-md w-[36%] max-lg:w-[92%] pb-[2rem] rounded-lg`}>
         <header  className='bg-white rounded-lg flex justify-between px-4 py-4'>
             <div className='flex flex-col gap-3'>
-        <h2 className='font-bold text-lg'>Name of Repository</h2>
+        <h2 className='font-bold text-xl max-lg:text-2xl  max-md:text-lg font-mono'>Name of Repository</h2>
         <h4>{props.name}</h4>
         </div>
         <MygitSvg/>
