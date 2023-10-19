a3f3e110b0d9394372acbcce713b463dd2df9852
Mr Fynn <104089609+MrFynnA@users.noreply.github.com>
2023-10-19 10:19:29 +0000

    Update gitRepoList.jsx

--- src/components/gitRepoList.jsx
+++ src/components/gitRepoList.jsx
@@ -5,7 +5,7 @@ import Header from './Header'
 export default function GitRepoList({data}) {
   return (
     <>
-    <div className='flex justify-center mt-10 pb-16'>
+    <div className='flex justify-center mt-10 max-md:mt-6 pb-16'>
     <div className='flex w-[100%] justify-center flex-wrap gap-x-10'>
         {data.map(dataItem=><GitHubRepos
                     key={dataItem.id}
